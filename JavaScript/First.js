882b3f11fb9099092ff994690e743cc9ae407db4
Y-shu <nivik12292@gmail.com>
2018-06-30 13:09:05 +0530

    First JavaScript(Alert,console,prompt,window.alert)....

---
+++ JavaScript/First.js
@@ -0,0 +1,7 @@
+console.log("Hello World Of JavaScript")
+alert("Hello Java Script")
+window.alert("Hello")
+var number = prompt("Enter a number:")
+console.log(number);
+var flag=confirm("Are you ?")
+console.log(flag);
