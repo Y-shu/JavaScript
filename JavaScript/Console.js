dcbc77cb9933982b25a68117334fa7fbff435a06
Y-shu <nivik12292@gmail.com>
2018-07-02 15:36:03 +0530

    2thJuly@2018-Variables,Constants...

---
+++ JavaScript/Console.js
@@ -0,0 +1,15 @@
+var number = 5;
+let message = "The Global Message for Humanity"
+console.log("The value is :",number);
+console.log(message);
+var number = "Five"; //redeclaring a variable
+console.log("The value is :",number);
+
+let no=201;
+console.log(no);
+
+no="Two hundard one";
+console.log(no);
+
+const fixed = "This is fixed value";
+console.log(fixed);
---
+++ JavaScript/type.js
@@ -0,0 +1,9 @@
+"use strict";
+var no = 5;
+console.log("The value",no);
+console.log("Type",typeof no);
+console.log("Type",typeof(no));
+var str = "Five";
+console.log(str);
+console.log("Type"+str+"More string Here");
+console.log("Type",typeof(str));
