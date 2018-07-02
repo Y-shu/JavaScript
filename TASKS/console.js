b51a254c906e4b73c97667e2791a1a4dd79159a3
Y-shu <nivik12292@gmail.com>
2018-07-02 15:53:12 +0530

    TASK2ndJULY-Alert....

---
+++ TASKS/console.js
@@ -0,0 +1,9 @@
+"use strict"
+var Name = prompt("Name")
+console.log(Name);
+var Email= prompt("Email")
+console.log(Email);
+var Password = prompt("Password")
+console.log(Password);
+var Age = prompt("Age")
+console.log(Age);
