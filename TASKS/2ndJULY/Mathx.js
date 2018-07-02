403e915333d4ce033d5b850fd4a5efde332df5a9
Y-shu <nivik12292@gmail.com>
2018-07-02 16:25:15 +0530

    2ndJULYTASK@...Roots(sqrt and cbrt)...

---
+++ TASKS/2ndJULY/Mathx.js
@@ -0,0 +1,9 @@
+//"use strict"
+var sqr = 10;
+console.log(sqr*sqr);
+var cub = 2;
+console.log(cub*cub*cub);
+var sqroot = 144;
+console.log(Math.sqrt(sqroot)); //Math.sqrt is method to cal square root
+var cuberoot = 27;
+console.log(Math.cbrt(cuberoot)); //Math.cbrt is method to cal cube sqroot
