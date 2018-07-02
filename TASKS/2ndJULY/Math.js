8cf75b309cd4473133b36aca75ea6ae8b7b63750
Y-shu <nivik12292@gmail.com>
2018-07-02 16:05:57 +0530

    2ndJULYTASK@Math....

---
+++ TASKS/2ndJULY/Math.js
@@ -0,0 +1,13 @@
+"use strict"
+var num1 = 10;
+console.log(num1);
+var num2 = 20;
+console.log(num2);
+var add = num1+num2;
+console.log(add);
+var sub = num1-num2;
+console.log(sub);
+var mul = num1*num2;
+console.log(mul);
+var div = num1/num2;
+console.log(div);
