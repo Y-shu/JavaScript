09fb33d6bb66689fe62c08fdfb83844aae62818c
Y-shu <nivik12292@gmail.com>
2018-07-09 16:14:58 +0530

    Arrays....Methods in arrays....

---
+++ JavaScript/array.js
@@ -0,0 +1,26 @@
+let name = ['John','Sam','Ram','Rohn'];
+let John = ['John','Samith',25,true,250000.23,null,name];//we can add array inside an array
+let rohn = [John,name];
+console.log(John);
+console.log(typeof John);
+John[3] = false; //directly replacing...
+console.log(John);
+John.push("Blue"); //method to add element at the end
+John.unshift("Mr."); //method to add element at the begining
+// John.pop();// Method to remove element from the last
+// John.shift();// Method to remove element from the begining
+console.log(John);
+console.log(John.indexOf('Samith')); // method returens either 1 or -1
+
+//console.log("Name :"+John[0]);
+//console.log("Salary :"+John[4]);
+//for(let index=0;index<John.length;index++)
+//console.log(John[5][2])
+// for(let index=0;index<rohn.length;index++)
+// {
+//   console.log(John[index]);
+//   for(let nIndex=0;nIndex<rohn[index].length;nIndex++)
+//   {
+//     console.log(rohn[index][nIndex]);
+//   }
+// } //counter-for execute till particular count
