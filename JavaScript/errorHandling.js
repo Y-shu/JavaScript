e2b19c9892fd8bf928109e6f51cff1f4f3cbc5d1
Y-shu <nivik12292@gmail.com>
2018-07-24 12:56:43 +0530

    class-ErrorHandling...try,catch,finally

---
+++ JavaScript/errorHandling.js
@@ -0,0 +1,23 @@
+// try{ //first executed
+//   //logic
+//   alert('Start of program!!' );
+//   agftsygj
+//   //try block is to handle error with some logic
+// }catch(error){ //second executed
+//   alert('Error of the Program');
+// }
+// alert('End of program!');
+
+try{ //first executed
+  //logic
+  alert('Start of program!!' );
+  agftsygj
+  //try block is to handle error with some logic
+}catch(error){ //second executed
+  //catching error and displays message
+  alert('Error of the Program');
+}finally{
+  //always execute even you are having an err at a particular point in code
+  alert("This is always execute");
+}
+alert('End of program!');
